8437b5a9d7e0aaaf76e8711ca39aafa3e2d150d2
prabhu-raja <prabhuraja@gmail.com>
2020-05-22 22:27:05 -0400

    2️⃣ - Basic genre request

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
+const genres = [
+  { id: 1, name: 'Action 🚗 💨' },  
+  { id: 2, name: 'Horror 👹' },  
+  { id: 3, name: 'Romance 👩‍❤️‍👨' },  
+];
+
 app.get('/', (req, res) => {
-  res.send('Bello ✋');
-})
+  console.log(genres);
+  res.send(genres);
+});
 
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`🏎Listening on port: ${port}`));
