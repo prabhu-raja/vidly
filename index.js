59f257a2cdb09fcd55c65d07646c2b2c305d7724
prabhu-raja <prabhuraja@gmail.com>
2022-03-17 16:50:10 -0400

    basic

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Bello ✋');
-})
+const genres = [
+  { id: 1, name: 'Action 🚗 💨' },  
+  { id: 2, name: 'Horror 👹' },  
+  { id: 3, name: 'Romance 👩‍❤️‍👨' },  
+];
+
+app.get('/api/genres', (req, res) => {
+  console.log(genres);
+  res.send(genres);
+});
 
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`🏎Listening on port: ${port}`));
